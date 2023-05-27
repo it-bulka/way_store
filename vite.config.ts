98e9f0f9ed9d3e3b0b2d59b3b5ab2d5d5100de4f
Ivanna Liuba <iva147iva147@gmail.com>
2023-05-27 22:06:50 +0200

    added alias and globals for scss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import fileDirName from './config/utils/file_dir_name'
+
+const { __dirname } = fileDirName(import.meta.url)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src/'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "~/styles/_global.scss";`,
+      },
+    },
+  },
 })
