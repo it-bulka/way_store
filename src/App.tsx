bd4774e3b9d4ca0b7d1ffa1b98f3fda2b3efc421
Ivanna Liuba <iva147iva147@gmail.com>
2023-05-30 09:56:01 +0200

    added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Sidebar } from '@/components/ui/Sidebar/Sidebar'
 import { Products } from '@/components/business/Product/Products'
+import { Header } from '@/components/business/Header/Header'
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <main className="app-content">
         <Sidebar className="app-content-first" />
         <Products />
--- src/components/business/Header/Header.tsx
+++ src/components/business/Header/Header.tsx
@@ -1,3 +1,31 @@
-export const Header = () => {
-  return <header></header>
+import { type FC } from 'react'
+import cls from './Header.module.scss'
+import Logo from '@/assets/logo/logo.svg'
+import Heart from '@/assets/general/heart.svg'
+import Cart from '@/assets/general/cart.svg'
+import Person from '@/assets/general/person.svg'
+
+interface HeaderProps {
+  className?: string
+}
+export const Header: FC<HeaderProps> = ({ className = '' }) => {
+  return (
+    <div className={cls.header + ' ' + className}>
+      <div className={`${cls.logo} ${cls.col1}`}>
+        <Logo />
+      </div>
+      <div className={` ${cls.col2}`}></div>
+      <div className={`${cls.actions} ${cls.col3}`}>
+        <span>
+          <Heart />
+        </span>
+        <span>
+          <Cart />
+        </span>
+        <span>
+          <Person />
+        </span>
+      </div>
+    </div>
+  )
 }
