efe0889483b57ea01e3651811a2b5a1789aec106
Ivanna Liuba <iva147iva147@gmail.com>
2023-06-01 13:32:34 +0200

    added Button

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { type FC } from 'react'
+import cls from './Button.module.scss'
+
+interface ButtonProps {
+  className?: string
+  title: string
+}
+export const Button: FC<ButtonProps> = ({ className, title }) => {
+  return <div className={cls.button + ' ' + className}>{title}</div>
+}
