bee9082875f15814b3415db4d50deda704f01e75
IT.bulka <i.it.bulka@gmail.com>
2026-06-09 07:45:42 +0200

    fix(goods-loader): support category param to load correct product collection

--- src/pages/Goods/goodsLoader.ts
+++ src/pages/Goods/goodsLoader.ts
@@ -1,9 +1,10 @@
 import type { LoaderFunctionArgs } from 'react-router-dom'
 import { getDocInfo } from '@/services'
 import { PAGES } from '@/models'
-import type { IProduct } from '@/models/goodsType'
+import type { IProduct, ProductType } from '@/models/goodsType'
 
-export const goodsLoader = async ({ params }: LoaderFunctionArgs) => {
-  if (params.slug) return await getDocInfo<IProduct>(PAGES.getCollection(), params.slug)
-  return null
+export const goodsLoader = async ({ params, request }: LoaderFunctionArgs) => {
+  if (!params.slug) return null
+  const category = (new URL(request.url).searchParams.get('category') as ProductType) ?? 'rings'
+  return getDocInfo<IProduct>(PAGES.getCollection(undefined, category), params.slug)
 }
