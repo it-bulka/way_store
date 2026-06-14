d8c9007b347592d02662831c040239266fab549f
IT.bulka <i.it.bulka@gmail.com>
2026-06-14 08:06:24 +0200

    feat(seo): add OG and Twitter Card meta tags with fallback image

--- src/components/ui/PageMeta/PageMeta.tsx
+++ src/components/ui/PageMeta/PageMeta.tsx
@@ -1,5 +1,8 @@
 import { Helmet } from 'react-helmet-async'
 
+const SITE_URL = 'https://way-store.netlify.app'
+const DEFAULT_OG_IMAGE = `${SITE_URL}/og-image.jpg`
+
 interface PageMetaProps {
   title: string
   description?: string
@@ -7,15 +10,29 @@ interface PageMetaProps {
   noindex?: boolean
 }
 
-export const PageMeta = ({ title, description, ogImage, noindex }: PageMetaProps) => (
-  <Helmet>
-    <title>{title} | Way</title>
-    {description && <meta name="description" content={description} />}
-    {noindex && <meta name="robots" content="noindex, nofollow" />}
-    <meta property="og:site_name" content="Way" />
-    <meta property="og:title" content={`${title} | Way`} />
-    {description && <meta property="og:description" content={description} />}
-    <meta property="og:type" content={ogImage ? 'product' : 'website'} />
-    {ogImage && <meta property="og:image" content={ogImage} />}
-  </Helmet>
-)
+export const PageMeta = ({ title, description, ogImage, noindex }: PageMetaProps) => {
+  const image = ogImage ?? DEFAULT_OG_IMAGE
+  const url = typeof window !== 'undefined' ? window.location.href : SITE_URL
+
+  return (
+    <Helmet>
+      <title>{title} | Way</title>
+      {description && <meta name="description" content={description} />}
+      {noindex && <meta name="robots" content="noindex, nofollow" />}
+
+      <meta property="og:site_name" content="Way" />
+      <meta property="og:type" content={ogImage ? 'product' : 'website'} />
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={`${title} | Way`} />
+      {description && <meta property="og:description" content={description} />}
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={`${title} | Way`} />
+      {description && <meta name="twitter:description" content={description} />}
+      <meta name="twitter:image" content={image} />
+    </Helmet>
+  )
+}
