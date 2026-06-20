ac4a9974ac65badacfb11d304a07121935300e9f
IT.bulka <i.it.bulka@gmail.com>
2026-06-20 11:38:07 +0200

    refactor(page): update NotFound i18n setup

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,8 +3,10 @@ import Logo from '@/assets/logo/logo.svg'
 import { AppLink } from '@/components/ui/AppLink/AppLink'
 import { Typography, TypographyTypes } from '@/components/ui/Typography/Typography'
 import { PageMeta } from '@/components/ui/PageMeta/PageMeta'
+import { useTranslation } from 'react-i18next'
 
 const NotFound = () => {
+  const { t } = useTranslation('common')
   return (
     <div className={cls.notFound}>
       <PageMeta title="Сторінку не знайдено" noindex />
@@ -12,15 +14,17 @@ const NotFound = () => {
         <div className={cls.logo}>
           <Logo />
         </div>
-        <Typography type={TypographyTypes.HEADER} variant="h1" className={cls.info}>
-          Page Not Found
-        </Typography>
-        <AppLink
-          title="Вернуться на главную"
-          path="/"
-          withDecoration={false}
-          className={cls.link}
-        />
+        <div className="container">
+          <Typography type={TypographyTypes.HEADER} variant="h1" className={cls.info}>
+            {t('notFound.title')}
+          </Typography>
+          <AppLink
+            title={t('notFound.backHome')}
+            path="/"
+            withDecoration={false}
+            className={cls.link}
+          />
+        </div>
       </div>
     </div>
   )
